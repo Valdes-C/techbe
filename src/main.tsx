c1ffdb83f1318c268120a5f2a8293b8738d3255a
Krzysztof Spaliński <kspalinski@tarczynski.pl>
2026-03-19 11:42:23 +0100

    basics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+
 import "./index.css";
-import App from "./App.tsx";
+
+import Basics from "./basics.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Basics/>
   </StrictMode>,
 );
